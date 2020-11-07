95b2cdeeec5ae787d6c9583c0764634b91f6402c
Guilherme Moriggi de Souza <guilhermemoriggidesouza@gmail.com>
2020-11-07 17:59:25 -0300

    create first build

---
+++ build/precache-manifest.709bbf820243bb88bf00eecde3789172.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b46ba0591dab3e3b5dfb84f4c5bcef58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b151c9e2a43de8ca14be",
+    "url": "/static/js/2.9a3ccfd8.chunk.js"
+  },
+  {
+    "revision": "8eb54d3d550627f18d1da5e9054f6210",
+    "url": "/static/js/2.9a3ccfd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d73dab46cfe4e09c59",
+    "url": "/static/js/main.9540de80.chunk.js"
+  },
+  {
+    "revision": "d0d174890ded2ae5a0da",
+    "url": "/static/js/runtime-main.26e7b110.js"
+  },
+  {
+    "revision": "1e333b7edf001fca7e571034dcd91053",
+    "url": "/static/media/lineto-circular-black.1e333b7e.otf"
+  },
+  {
+    "revision": "47be2e4ae08dbe1c015c0126ba7487af",
+    "url": "/static/media/lineto-circular-black.47be2e4a.ttf"
+  },
+  {
+    "revision": "7ec8e7208b2f7acf284f63e7966bad5b",
+    "url": "/static/media/lineto-circular-black.7ec8e720.woff"
+  },
+  {
+    "revision": "b350861726a8170c9a605f9efb8ae68f",
+    "url": "/static/media/lineto-circular-black.b3508617.svg"
+  },
+  {
+    "revision": "e6a159cec2fb23eb76374e7641b77a6f",
+    "url": "/static/media/lineto-circular-black.e6a159ce.woff2"
+  },
+  {
+    "revision": "9ab0d4f6e6d44bc63831d4cd104d1162",
+    "url": "/static/media/logo.9ab0d4f6.svg"
+  }
+]);
